39a3bbdf417dae4299e8d1e60c527c0fdebeaa4b
dimi88 <kurukii88@gmail.com>
2015-01-29 23:30:17 +0100

    sketch that demonstrates basic automated movement, utilizing the magnetometer

---
+++ examples/basic_automated_movement/basic_automated_movement.ino
@@ -0,0 +1,21 @@
+#include <AFMotor.h>
+#include <Smartcar.h>
+#include <Wire.h>
+#include <HMC5883L.h>
+
+Smartcar elsa;
+
+void setup() {
+    elsa.begin();
+    elsa.goForward(60);
+    elsa.rotateClockwise(90);
+    elsa.goForward(60);
+    elsa.rotateCounterClockwise(90);
+    elsa.goBackward(60);
+    elsa.rotateCounterClockwise(90);
+    elsa.goForward(60);
+    elsa.rotateCounterClockwise(270);
+}
+
+void loop() {
+}
