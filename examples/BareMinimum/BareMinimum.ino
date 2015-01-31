ac37bfe018903ba7c489611b2dadf908a4bb720a
dimi88 <kurukii88@gmail.com>
2015-01-31 11:52:47 +0100

    Minimum example sketch to get working with the library

---
+++ examples/BareMinimum/BareMinimum.ino
@@ -0,0 +1,14 @@
+#include <AFMotor.h>
+#include <Smartcar.h>
+#include <Wire.h>
+#include <HMC5883L.h>
+
+Smartcar simeon;
+
+void setup(){
+  simeon.begin();
+  /* put your setup code here to run once */
+}
+void loop(){
+  /* your main loop code here to run repeatedly */
+}
